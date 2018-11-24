4f19010fab1a11fe02fa66cab6c8759e75be7fea
Quang Hung <quanghungleo@gmail.com>
2018-11-24 10:04:37 +0700

    First

---
+++ FAIC/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FAIC.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int SupplierID { get; set; }
+        public int CategoryID { get; set; }
+        public int QuantityPerUnit { get; set; }
+        public int UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+        public int UnitOnOrder { get; set; }
+        public int ReorderLevel { get; set; }
+        public string Discontinued { get; set; }
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+    }
+}
